2b75530d81d46ab6b6ca9430402db0e626c8a4cf
matomaylla <michael.tomaylla@pucp.edu.pe>
2024-10-30 01:33:32 +0000

    add package info

--- src/main/java/com/matomaylla/product/ProductApplication.java
+++ src/main/java/com/matomaylla/product/ProductApplication.java
@@ -3,11 +3,21 @@ package com.matomaylla.product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Clase principal de la aplicación Spring Boot.
+ */
 @SpringBootApplication
 public class ProductApplication {
+	
+	// Constructor privado para evitar instanciación
+	private ProductApplication() {}
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProductApplication.class, args);
+	/**
+	 * Método principal que inicia la aplicación Spring Boot.
+	 *
+	 * @param args argumentos de la línea de comandos
+	 */
+	public static void main(final String[] args) {
+	    SpringApplication.run(ProductApplication.class, args);
 	}
-
 }
