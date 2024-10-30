af2fa9ddafe8b748186449ebba2035a1445c3fef
Michael Antonio Tomaylla Mendoza <pulser2310@gmail.com>
2024-10-30 20:57:37 +0000

    add data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO product (name, price) VALUES ('Laptop', 1500.0);
+INSERT INTO product (name, price) VALUES ('Smartphone', 800.0);
+INSERT INTO product (name, price) VALUES ('Tablet', 500.0);
+INSERT INTO product (name, price) VALUES ('Headphones', 150.0);
+INSERT INTO product (name, price) VALUES ('Monitor', 300.0);
